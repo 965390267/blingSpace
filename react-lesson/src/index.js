debd087e4fc086b26a2687faf6c5bb8c6766662b
wzy <3101885298@qq.com>
2020-10-05 18:14:27 +0800

    生命周期

--- react-lesson/src/index.js
+++ react-lesson/src/index.js
@@ -1,78 +1,83 @@
-import React, {Component} from 'react';
-import ReactDom,{render} from 'react-dom';
+import React from 'react';
+import ReactDOM,{render} from 'react-dom';
 
-import MessageBox from './components/MessageBox';
-import MessageInput from './components/MessageInput';
 
-import 'bootstrap/dist/css/bootstrap.css';
-import axios from './axios';
-
-import {Consumer,Provider} from './context';
-class App extends Component {
-    state = {
-        messageList: [],
-        total: 0
-    }
-    // 在此获取数据 获取后把数据传递给messageBox
+class ChildCounter extends React.Component {
+    state = {}
     componentWillMount(){
+        console.log('son componentWillMount');
     }
-    async componentDidMount() {
-       try {
-            let data = await axios.get('/user.json');
-            console.log(data);
-            this.setState({
-                messageList: data
-            })
-       } catch (error) {
-            console.log("======",error);
-       }
+    componentDidMount(){
+        console.log('son componentDidMount');
     }
-    add = ()=>{
-        this.setState({
-            total:this.state.total+1
-        })
+    componentWillReceiveProps(){
+
     }
-    addMessage = (val) => {
-        let m = {
-            avatar: 'http://pic.sc.chinaz.com/Files/pic/pic9/202009/apic28073_s.jpg',
-            message:val,
-            time: Date.now()
-        }
-        this.setState({
-            messageList: [
-                ...this.state.messageList,m
-            ]
-        })
+    getSnapshotBeforeUpdate(){
+
+    }
+    static getDerivedStateFromProps(){
+
     }
     render() {
-        console.log('render')
-       return (
-            <Provider value={{
-                add:this.add,
-                a:1
-            }}>
-                <div className="container">
-                    <div className="panel panel-primary">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Panel title</h3>
-                        </div>
-                        <div className="panel-body">
-                        <MessageBox messageList={this.state.messageList}></MessageBox>
-                        </div>
-                    </div>
-                    <div className="panel-footer">
-                        <MessageInput addMessage={this.addMessage}></MessageInput>
-                    </div>
-
-                    <h2>总点赞数 {this.state.total} </h2>
-                </div>
-
-            </Provider>
+        console.log('son render');
+        return (
+            <div>
+                子
+            </div>
         )
     }
 }
 
-render(
-    <App></App>,
-    window.root
-)
+class Counter extends React.Component {
+    static defaultProps = {}
+    state = {
+        count: 0
+    }
+    constructor(){
+        super()
+        console.log('constructor');
+
+    }
+    componentWillMount(){
+        console.log('componentWillMount');
+    }
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+    // 解决状态未改变却还执行render的问题  PureComponent本质上只是重写了这个方法
+    shouldComponentUpdate(nextProps,nextState) {
+        console.log(nextState);
+        return nextState.count == this.state.count ? false : true;
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate');
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate')
+    }
+    componentWillReceiveProps(){
+        console.log('componentWillReceiveProps');
+    }
+    handleClick = () => {
+        this.setState({
+            count:this.state.count+1
+        })
+    }
+    render() {
+        console.log('render');
+        return (
+            <div>
+                <button onClick={this.handleClick}>增加</button>
+                <ChildCounter></ChildCounter>
+            </div>
+        )
+    }
+}
+// constructor
+// componentWillMount    (此钩子函数因存在多次调用的可能，已被react废弃)
+// render
+// componentDidMount
+render(<Counter>
+
+</Counter>,window.root)
